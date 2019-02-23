0543dba7de250b5122dbabb2ee324b7069ef290a
rob <robertaxelkirby@gmail.com>
2019-02-23 16:24:22 +0000

    Add basic validation for post request

--- server/index.js
+++ server/index.js
@@ -4,18 +4,30 @@ const cors = require('cors');
 const port = 5001;
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+
+const isValid = (tweet) => {
+  return tweet.name && tweet.name.toString().trim() !== '';
+}
 
 app.get('/', (req, res) => {
   res.json({
     message: 'Request received'
-  })
-})
+  });
+});
 
 app.post('/tweets', (req, res) => {
-  console.log(req.body);
-})
+  if (isValid(req.body)) {
+    // insert into db..
+    const tweet = {
+      name: req.body.name.toString(),
+      content: req.body.content.toString(),
+    };
+  } else {
+    res.json({ message: 'Name and content are required!'});
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is listening on http://localhost:${port}`);
