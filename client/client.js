bb6aefa445e1aac8788e9da091f2f4f1a4baeabe
rob <robertaxelkirby@gmail.com>
2019-02-23 15:41:58 +0000

    Select form contents in js

--- client/client.js
+++ client/client.js
@@ -1,3 +1,10 @@
 const form = document.querySelector('form');
 
-console.log(form);
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const name = formData.get('name');
+  const content = formData.get('content');
+  const tweet = { name, content };
+  console.log(tweet);
+});
