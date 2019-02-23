a5a4611085f17d240f541fd80cb5a3ae10c0bf9c
rob <robertaxelkirby@gmail.com>
2019-02-23 15:57:05 +0000

    Add get route

--- server/index.js
+++ server/index.js
@@ -3,6 +3,12 @@ const express = require('express');
 const port = 5001;
 const app = express();
 
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Request received'
+  })
+})
+
 app.listen(port, () => {
   console.log(`Server is listening on http://localhost:${port}`);
 });
