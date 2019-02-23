994b3bf60b432faa8b7b7d6e3aefeb255f60c552
rob <robertaxelkirby@gmail.com>
2019-02-23 16:57:19 +0000

    Add fetch for all tweets on pageload

--- client/client.js
+++ client/client.js
@@ -1,7 +1,15 @@
 const form = document.querySelector('form');
+const body = document.querySelector('body');
+const tweetsElement = document.querySelector('.tweets');
 const loadingElement = document.querySelector('.loading');
-const POST_URL = 'http://localhost:5001/tweets';
+const API_URL = 'http://localhost:5001/tweets';
 
+body.onload = fetch(API_URL, { method: 'GET' })
+  .then(response => response.json())
+  .then(tweets => {
+    console.log(tweets);
+  })
+  .catch(err => console.error(err));
 
 loadingElement.style.display = 'none';
 
@@ -15,18 +23,18 @@ form.addEventListener('submit', (event) => {
   form.style.display = 'none';
   loadingElement.style.display = '';
 
-  fetch(POST_URL, {
+  fetch(API_URL, {
     method: 'POST',
     body: JSON.stringify(tweet),
     headers: {
       'Content-Type': 'application/json',
     },
   })
-  .then(response => response.json())
-  .then(createdTweet => {
-    console.log(createdTweet);
-    form.style.display = '';
-    loadingElement.style.display = 'none';
-  })
-  .catch((err) => console.error(err));
+    .then(response => response.json())
+    .then(createdTweet => {
+      console.log(createdTweet);
+      form.style.display = '';
+      loadingElement.style.display = 'none';
+    })
+    .catch((err) => console.error(err));
 });
