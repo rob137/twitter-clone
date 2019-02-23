502b64f3ef12d181dd50214f80fe831a1092269c
rob <robertaxelkirby@gmail.com>
2019-02-23 15:43:52 +0000

    Replace form with loading spinner on submission

--- client/client.js
+++ client/client.js
@@ -1,4 +1,7 @@
 const form = document.querySelector('form');
+const loadingElement = document.querySelector('.loading');
+
+loadingElement.style.display = 'none';
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -7,4 +10,6 @@ form.addEventListener('submit', (event) => {
   const content = formData.get('content');
   const tweet = { name, content };
   console.log(tweet);
+  form.style.display = 'none';
+  loadingElement.style.display = '';
 });
