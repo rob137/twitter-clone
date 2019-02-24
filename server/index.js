ddefc61ae0996b99189e0661a2e5a4d20d15ac11
rob <robertaxelkirby@gmail.com>
2019-02-24 09:14:52 +0000

    Make server use express-rate-limit

--- server/index.js
+++ server/index.js
@@ -1,16 +1,22 @@
-const express = require('express');
+2const express = require('express');
 const cors = require('cors');
 const monk = require('monk');
 const Filter = require('bad-words');
+const rateLimit = require('express-rate-limit');
 
 const port = 5001;
 const app = express();
 const db = monk('localhost/tweet-clone');
 const tweets = db.get('tweets');
 const filter = new Filter();
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 300 // limite each IP to 300 requests per windowMs
+})
 
 app.use(cors());
 app.use(express.json());
+app.use (limiter);
 
 const isValid = (tweet) => {
   return tweet.name && tweet.name.toString().trim() !== '';
@@ -30,7 +36,7 @@ app.get('/tweets', (req, res) => {
 
 app.post('/tweets', (req, res) => {
   if (isValid(req.body)) {
-    // insert into db..
+    // sanitise swearwords and insert into db..
     const tweet = {
       name: filter.clean(req.body.name.toString()),
       content: filter.clean(req.body.content.toString()),
